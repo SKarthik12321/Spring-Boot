f006a07eb0e09373e61a8a7a9d107dcd39d17d35
S KARTHIK <skarthik.link@gmail.com>
2026-03-11 14:00:59 +0530

    feat(uc3): add handler to process query parameters

--- src/main/java/tomcatservlet/SimpleHttpServer.java
+++ src/main/java/tomcatservlet/SimpleHttpServer.java
@@ -1,8 +1,8 @@
 package tomcatservlet;
 
 import com.sun.net.httpserver.HttpServer;
-import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
 import java.io.OutputStream;
@@ -12,47 +12,64 @@ public class SimpleHttpServer {
 
     public static void main(String[] args) throws Exception {
 
-        HttpServer server = HttpServer.create(new InetSocketAddress(9000), 0);
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
 
+        // UC1
         server.createContext("/", new RootHandler());
+
+        // UC2
         server.createContext("/headers", new HeaderHandler());
+
+        // UC3
+        server.createContext("/query", new QueryHandler());
 
         server.setExecutor(null);
         server.start();
 
-        System.out.println("Server started at http://localhost:9000");
+        System.out.println("Server started at http://localhost:8080");
     }
 
+    // UC1
     static class RootHandler implements HttpHandler {
-
         public void handle(HttpExchange exchange) throws IOException {
-
-            String response = "Java Web Server Running";
-
+            String response = "Hello from Simple Java HTTP Server";
             exchange.sendResponseHeaders(200, response.length());
-
             OutputStream os = exchange.getResponseBody();
             os.write(response.getBytes());
             os.close();
         }
     }
-    static class HeaderHandler implements HttpHandler {
 
+    // UC2
+    static class HeaderHandler implements HttpHandler {
         public void handle(HttpExchange exchange) throws IOException {
 
             StringBuilder response = new StringBuilder();
 
-            for (String key : exchange.getRequestHeaders().keySet()) {
-                response.append(key)
-                        .append(" : ")
-                        .append(exchange.getRequestHeaders().get(key))
-                        .append("\n");
-            }
+            exchange.getRequestHeaders().forEach((key,value) ->
+                    response.append(key).append(" : ").append(value).append("\n")
+            );
+
+            byte[] bytes = response.toString().getBytes();
+
+            exchange.sendResponseHeaders(200, bytes.length);
+            OutputStream os = exchange.getResponseBody();
+            os.write(bytes);
+            os.close();
+        }
+    }
+
+    // UC3
+    static class QueryHandler implements HttpHandler {
+        public void handle(HttpExchange exchange) throws IOException {
+
+            String query = exchange.getRequestURI().getQuery();
+            String response = "Query received: " + query;
 
             exchange.sendResponseHeaders(200, response.length());
 
             OutputStream os = exchange.getResponseBody();
-            os.write(response.toString().getBytes());
+            os.write(response.getBytes());
             os.close();
         }
     }
