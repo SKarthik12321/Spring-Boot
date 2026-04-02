dbb9fcddcf5acdb928dc09ff2b30ac75decc3fc7
S KARTHIK <skarthik.link@gmail.com>
2026-04-02 16:37:04 +0530

    Add Tomcat Servlet implementation

--- src/main/java/tomcatservlet/LoginServlet.java
+++ src/main/java/tomcatservlet/LoginServlet.java
@@ -1,43 +1,25 @@
 package tomcatservlet;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.annotation.WebServlet;
-
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
 
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    protected void doPost(HttpServletRequest request,
+                          HttpServletResponse response)
             throws ServletException, IOException {
+
+        String user = request.getParameter("user");
+        String pwd = request.getParameter("pwd");
 
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
 
-        out.println("<form method='post' action='login'>");
-        out.println("Username: <input type='text' name='username'/><br>");
-        out.println("Password: <input type='password' name='password'/><br>");
-        out.println("<input type='submit' value='Login'/>");
-        out.println("</form>");
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
-
-        response.setContentType("text/html");
-
-        PrintWriter out = response.getWriter();
-
-        if ("admin".equals(username) && "admin123".equals(password)) {
+        if ("admin".equals(user) && "1234".equals(pwd)) {
             out.println("<h2>Login Successful</h2>");
         } else {
             out.println("<h2>Invalid Credentials</h2>");
